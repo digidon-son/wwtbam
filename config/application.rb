26da480c18f5adee9e64d68fe6482cfa4c7befa5
digidon-son <e.lepehin93@gmail.com>
2022-05-30 15:05:18 +0300

    added local, fallback, and time_zone

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,21 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module Billionaire
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    # Initialize configuration defaults for originally generated Rails versio
+    config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
     config.i18n.default_locale = :ru
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.locale = :ru
+    config.i18n.fallbacks = [:en]
+
+    config.time_zone = 'Moscow'
   end
 end
